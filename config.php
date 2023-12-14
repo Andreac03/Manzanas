025f84683d38f1205d6b2af91d0d274afc27cb66
josedavid0793 <32796775+josedavid0793@users.noreply.github.com>
2023-12-14 13:58:50 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+define("key", "cristian");
+define("cod", "AES-128-ECB");
+
+define("servidor", "localhost");
+define("usuario","root");
+define("password","");
+define("db","Manzanas");
+?>
